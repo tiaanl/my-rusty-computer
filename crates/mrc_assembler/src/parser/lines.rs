0c0bebb93a7d518e62cfeebbc5d6735bb0004c10
Tiaan Louw <tiaanl@gmail.com>
2022-01-29 08:29:00 +0100

    Test the source from the samples directory.

--- crates/mrc_assembler/src/parser/lines.rs
+++ crates/mrc_assembler/src/parser/lines.rs
@@ -437,48 +437,9 @@ mod parse {
 
         #[test]
         fn parse_program() {
-            let source = r"
-                push    bx
-                push    cx
-                push    dx
-                push    si
-                push    di
+            let source = String::from_utf8_lossy(include_bytes!("../../../../samples/strcmp.asm"));
 
-            loop_strcmp_loop1:
-                mov     al, byte cs:[si]
-                inc     si
-                cmp     al, bl
-                jbe     loop_strcmp_great
-                cmp     al, bl
-                jl      loop_strcmp_less
-                cmp     al, '$'
-                je      loop_strcmp_quit
-                cmp     bl, '$'
-                je      loop_strcmp_quit
-                jmp     loop_strcmp_loop1
-
-            loop_strcmp_quit:
-                mov     al, 1
-                jmp     done
-
-            loop_strcmp_great:
-                mov     al, 2
-                jmp     done
-
-            loop_strcmp_less:
-                mov     al, 0
-                jmp     done
-
-            done:
-                pop     di
-                pop     si
-                pop     dx
-                pop     cx
-                pop     bx
-                ret
-            ";
-
-            let p = program(Span::new(source));
+            let p = program(Span::new(source.as_ref()));
             println!("{:#?}", p);
         }
     }
