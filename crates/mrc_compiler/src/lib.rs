7f2b9795fcc84c62cb2fca302deb865dd318e7e1
Tiaan Louw <tiaanl@gmail.com>
2022-11-27 10:06:59 +0100

    Add Display and span() functions to CompileError.

--- crates/mrc_compiler/src/lib.rs
+++ crates/mrc_compiler/src/lib.rs
@@ -12,6 +12,24 @@ pub enum CompileError {
     CompileError(compiler::CompileError),
 }
 
+impl CompileError {
+    pub fn span(&self) -> &ast::Span {
+        match self {
+            CompileError::ParserError(err) => err.span(),
+            CompileError::CompileError(err) => err.span(),
+        }
+    }
+}
+
+impl std::fmt::Display for CompileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CompileError::ParserError(err) => err.fmt(f),
+            CompileError::CompileError(err) => err.fmt(f),
+        }
+    }
+}
+
 pub fn compile(source: &str) -> Result<Vec<u8>, CompileError> {
     let mut parser = parser::Parser::new(source);
     let mut compiler = compiler::Compiler::default();
